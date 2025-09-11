831f860880d73f675cd1fcb46d9a86139e0771d5
Yahir9999 <angelguzman.vna@gmail.com>
2025-09-11 13:29:52 -0600

    Mi primer deploy

--- Aplicativo . Evidencias/app.js
+++
@@ -1,9 +0,0 @@
-// Registrar el service worker 
-if("serviceWorker" in navigator){
-    navigator.serviceWorker.register("/serviceWorker.js")
-    .then((reg) => console.log("Service worker registrado:", reg))
-    .catch((err) => console.log("Error de registro de service worker:", err));
-}
-
-// Mensaje de validacion
-console.log("App iniciada");
